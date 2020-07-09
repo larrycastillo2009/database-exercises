94575c1acbcaeb2f7787521d79b261f10d64406f
Larry Castillo <larrycastillo2009@gmail.com>
2020-07-09 10:16:48 -0500

    group by exercise completion

---
+++ group_by_exercises.sql
@@ -0,0 +1,39 @@
+USE employees;
+
+
+# 2
+SELECT DISTINCT title FROM titles;
+
+# 3
+SELECT last_name
+FROM employees
+WHERE last_name LIKE 'e%e'
+GROUP BY last_name
+
+# 4
+SELECT last_name
+FROM employees
+WHERE last_name LIKE 'e%e'
+GROUP BY first_name, last_name
+
+# 5
+SELECT last_name
+FROM employees
+WHERE last_name LIKE '%q%'
+AND last_name NOT LIKE '%qu%'
+GROUP BY last_name
+
+# 6
+SELECT COUNT(last_name)
+FROM employees
+WHERE last_name LIKE '%q%'
+AND last_name NOT LIKE '%qu%'
+ORDER BY last_name;
+
+# 7
+SELECT COUNT(*), gender
+FROM employees
+WHERE first_name = 'Irena'
+    OR first_name = 'Vidya'
+    OR first_name = 'Maya'
+GROUP BY gender
